f93d25647445df612ea938ca4f6c61611594d563
John Moons <jmoons@realdigitalmedia.com>
2021-11-08 17:40:27 -0500

    First pass at collecting metrics of a text file

--- text_parser.rb
+++ text_parser.rb
@@ -1,3 +1,5 @@
+PUNCTUATION = /[!?.,:;(){}'"\[\]]/
+
 unless ARGV[0]
   puts "You must supply a file to parse as the first argument"
   exit
@@ -8,8 +10,24 @@ unless File.exist?( ARGV[0] )
   exit
 end
 
+word_hash = {}
+
 input_file = File.open( ARGV[0], "r" )
 input_file.each_line do | line |
-  puts line
+  trimed_line_array = line.chomp.gsub(PUNCTUATION, "").split(" ")
+
+  trimed_line_array.each do | word |
+    word = word.downcase
+    if word_hash[word]
+      word_hash[word] += 1
+    else
+      word_hash[word] = 1
+    end
+  end
+
 end
 input_file.close
+
+puts word_hash.sort_by{|word, word_count| word_count}.inspect
+puts "Word Count: #{word_hash.values.inject(0, :+)}"
+puts "Total Unique Words: #{word_hash.keys.length}"
